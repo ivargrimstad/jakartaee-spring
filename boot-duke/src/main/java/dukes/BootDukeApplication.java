e3054486fc1b2edf6528aa5e0f2e75a592846040
Ivar Grimstad <ivar.grimstad@eclipse-foundation.org>
2023-02-09 14:01:57 +0100

    Initial code

---
+++ boot-duke/src/main/java/dukes/BootDukeApplication.java
@@ -0,0 +1,13 @@
+package dukes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDukeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootDukeApplication.class, args);
+	}
+
+}
