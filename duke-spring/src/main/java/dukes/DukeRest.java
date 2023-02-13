428f244d37db4bab32b322228d7f98b723aee807
Ivar Grimstad <ivar.grimstad@eclipse-foundation.org>
2023-02-13 18:08:11 +0100

    Add use ServletContext

--- duke-spring/src/main/java/dukes/DukeRest.java
+++ duke-spring/src/main/java/dukes/DukeRest.java
@@ -1,19 +1,18 @@
 package dukes;
 
+import jakarta.servlet.ServletContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Collection;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 @RestController
 public class DukeRest {
 
+    @Autowired
+    private ServletContext context;
+
     @GetMapping("/hello")
-    public Collection<String> sayHello() {
-        return IntStream.range(0, 10)
-                .mapToObj(i -> "Hello number " + i)
-                .collect(Collectors.toList());
+    public String sayHello() {
+        return "Hello, Duke from " + context.getContextPath();
     }
 }
