224de5702ee3932718fae2a5fba908d7920959d5
Ivar Grimstad <ivar.grimstad@eclipse-foundation.org>
2023-02-13 17:53:24 +0100

    Restructure and clean up

--- duke-boot/src/main/java/dukes/DukeRest.java
+++ duke-boot/src/main/java/dukes/DukeRest.java
@@ -3,7 +3,7 @@ package dukes;
 import dukes.dep.Account;
 import dukes.dep.AccountRepository;
 import dukes.lib.User;
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
