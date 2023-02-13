460eb6e7f1f338e2f6e925d357603ab08684c656
Ivar Grimstad <ivar.grimstad@eclipse-foundation.org>
2023-02-13 14:54:04 +0100

    Add simple Spring web app to run on Tomcat

--- duke-spring/src/main/java/dukes/SpringDukeApplication.java
+++ duke-spring/src/main/java/dukes/SpringDukeApplication.java
@@ -2,12 +2,9 @@ package dukes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringDukeApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringDukeApplication.class, args);
-	}
+public class SpringDukeApplication extends SpringBootServletInitializer {
 
 }
