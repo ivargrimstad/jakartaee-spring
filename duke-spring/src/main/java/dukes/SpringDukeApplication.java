7be2493e68d9165ae180be57697efb641a06105d
Ivar Grimstad <ivar.grimstad@eclipse-foundation.org>
2023-02-15 08:14:11 -0800

    Clean up demo

--- duke-spring/src/main/java/dukes/SpringDukeApplication.java
+++ duke-spring/src/main/java/dukes/SpringDukeApplication.java
@@ -1,10 +1,9 @@
 package dukes;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class SpringDukeApplication extends SpringBootServletInitializer {
-
+    public static void main(String[] args) {}
 }
